b00f18601eafb7d902183e27ba460835b0cf866e
sami_bnt <samibentebbiche@gmail.com>
2020-05-31 12:32:35 +0100

    test7

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from stegano import app
 from PIL import Image
 import stegano.redimension, stegano.conversion, stegano.DCT_quantification,stegano.dissimulation_methode, stegano.dissimulation, stegano.psnr_mse, stegano.extraction_methode
 if __name__ == "__main__":
-   app.run(debug=True)
+   app.run()
 
