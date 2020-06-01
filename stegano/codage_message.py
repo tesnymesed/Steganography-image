b9a3d242a6c0a3a816d3915825bcf622fef83fd6
sami_bnt <samibentebbiche@gmail.com>
2020-06-01 21:36:51 +0100

    add

--- stegano/codage_message.py
+++ stegano/codage_message.py
@@ -2,32 +2,25 @@
 from random import random
 import math
 
-#ALPHABET=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-#				"1","2","3","4","5","6","7","8","9","0",
-#				" ","'","é","à",'"',"-","=","+","*","è","ç","#","&","<",">",",",".",";",":","!","?","@","(",")","{","}","/",'%',"[","]",'\\','\x11','ä',
-#				"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",'`','°','\r','\x9a','²','\n','\x0e','_','\x90']
-
-
 # fonction de chiffrement affine
 
-def chiffrementAffine(cle,L):
+def chiffrementAffine(cle,Lettre):
 
         
-        #x=ALPHABET.index(L)
-        x=ord(L)
+        x=ord(Lettre)
         
         y=(x+cle)%(1114112)
         
         return chr(y)
 
-# Calcul de l'inverse d'un nombre modulo (len(ALPHABET))
+
 
 # Fonction de déchiffrement
 
-def dechiffrementAffine(cle,L):
+def dechiffrementAffine(cle,Lettre):
 
-    #x=ALPHABET.index(L)
-    x=ord(L)
+    
+    x=ord(Lettre)
     
     y=((int(x)-int(cle)))%(1114112)
 
@@ -55,7 +48,7 @@ def decrypt(message,cle):
 
 
 def main():
-    message = 'bentebbiche'
+    message = 'BLIDA'
     cle = 5
 
     message_cry = crypt(message,cle)
